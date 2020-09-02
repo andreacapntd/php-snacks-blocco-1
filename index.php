8e9bd7982a583838c1a661b2b4b38e3e09eb03e3
andreacapntd <andreacapntd@gmail.com>
2020-09-02 17:00:14 +0200

    php snack 2 ok

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!-- Snack 1
 Creiamo un array contenente le partite di basket di un'ipotetica tappa del calendario. Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema. Olimpia Milano - Cantù | 55-60-->
 
-<?php
+<!-- <?php
 
  $matches = [
 
@@ -30,12 +30,32 @@ Creiamo un array contenente le partite di basket di un'ipotetica tappa del calen
 
    echo $match['team_home'] . ' - ' . $match['team_away'];
    echo ' / ';
-   echo $match['home_points'] . ' - ' . $match['away_points']; 
+   echo $match['home_points'] . ' - ' . $match['away_points'];
    echo '<br>';
  }
 
 
- ?>
+ ?> -->
 
 
 <!-- Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è ok stampare "Accesso riuscito", altrimenti "Accesso negato" -->
+
+
+<?php
+
+ $name = $_GET['name'];
+ $mail = $_GET['mail'];
+ $age = $_GET['age'];
+
+ if (strlen($name) > 3 && (strpos($mail, '.')) && (strpos($mail, '@')) && (is_numeric($age))) {
+
+   echo 'Login Successful';
+
+  } else {
+
+    echo "Access Denied!";
+
+  }
+
+
+?>
